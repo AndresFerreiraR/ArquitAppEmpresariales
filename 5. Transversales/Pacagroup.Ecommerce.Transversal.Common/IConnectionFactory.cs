a3de878be936d2758e600879c2f0454dd42eab80
AndresFerreiraR <ac.ferreira.r@gmail.com>
2023-02-05 23:38:01 -0500

    Add project files.

---
+++ 5. Transversales/Pacagroup.Ecommerce.Transversal.Common/IConnectionFactory.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Pacagroup.Ecommerce.Transversal.Common
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Data;
+    public interface IConnectionFactory
+    {
+        IDbConnection GetConnection { get; }
+    }
+}
