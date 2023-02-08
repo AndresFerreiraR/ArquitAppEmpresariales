c4fbf56f63b2375ce2f9556e82a4cdc8431dc011
AndresFerreiraR <ac.ferreira.r@gmail.com>
2023-02-07 23:28:14 -0500

    Integracion del Web Api con Swagger

--- 1.Servicio/Pacagroup.Ecommerce.Services.WebApi/Startup.cs
+++ 1.Servicio/Pacagroup.Ecommerce.Services.WebApi/Startup.cs
@@ -18,9 +18,12 @@ namespace Pacagroup.Ecommerce.Services.WebApi
     using Pacagroup.Ecommerce.Infraestructure.Repository;
     using Pacagroup.Ecommerce.Transversal.Common;
     using Pacagroup.Ecommerce.Transversal.Mapper;
+    using Swashbuckle.AspNetCore.Swagger;
     using System;
     using System.Collections.Generic;
+    using System.IO;
     using System.Linq;
+    using System.Reflection;
     using System.Threading.Tasks;
 
 
@@ -49,7 +52,31 @@ namespace Pacagroup.Ecommerce.Services.WebApi
             services.AddScoped<ICustomersDomain, CustomersDomain>();
             services.AddScoped<ICustomersRepository, CustomersRepository>();
 
-
+            // Configuracion Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "Pacagroup Technology Service API Market",
+                    Description = "A simple example .Net Core Project with DDD",
+                    TermsOfService = "None",
+                    Contact = new Contact
+                    {
+                        Email = "ac.ferreira.r@gmail.com",
+                        Name = "Andres Ferreira",
+                        Url = "https://github.com/AndresFerreiraR/ArquitAppEmpresariales.git"
+                    },
+                    License = new License
+                    {
+                        Name = "IMIT",
+                        Url = "https://example.com/license"
+                    }
+                });
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlFile);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -59,6 +86,13 @@ namespace Pacagroup.Ecommerce.Services.WebApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API ECommerce V1");
+            });
 
             app.UseMvc();
         }
