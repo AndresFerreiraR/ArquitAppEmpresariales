e6e00a190dad2ebe64390d1c21535d4f66949c0a
AndresFerreiraR <ac.ferreira.r@gmail.com>
2023-02-21 17:17:37 -0500

    Se realiza actualizacion de .Net5.0 a .Net6.0 y se actualiza swagger con openshift y esquema de seguridad

--- 1.Servicio/Pacagroup.Ecommerce.Services.WebApi/Modules/Swagger/SwaggerExtension.cs
+++ 1.Servicio/Pacagroup.Ecommerce.Services.WebApi/Modules/Swagger/SwaggerExtension.cs
@@ -1,9 +1,11 @@
 ﻿
 namespace Pacagroup.Ecommerce.Services.WebApi.Modules.Swagger
 {
+    using Microsoft.AspNetCore.Authentication.JwtBearer;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.OpenApi.Models;
     using System;
+    using System.Collections.Generic;
     using System.IO;
     using System.Reflection;
 
@@ -43,26 +45,37 @@ namespace Pacagroup.Ecommerce.Services.WebApi.Modules.Swagger
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
                 c.IncludeXmlComments(xmlPath);
 
-                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                var securityScheme = new OpenApiSecurityScheme
                 {
-                    Description = "Authorization by API key",
+                    Name = "Authorization",
+                    Description = "Enter JWT Bearer Token **_Only_**",
                     In = ParameterLocation.Header,
-                    Type = SecuritySchemeType.ApiKey,
-                    Name = "Authorization"
-                });
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    BearerFormat = "JWT",
+                    Reference = new OpenApiReference
+                    {
+                        Id = JwtBearerDefaults.AuthenticationScheme,
+                        Type = ReferenceType.SecurityScheme
+                    }
+                };
+
+                c.AddSecurityDefinition(securityScheme.Reference.Id, securityScheme);
 
                 c.AddSecurityRequirement(new OpenApiSecurityRequirement
                 {
                     {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            }
-                        },
-                        new string[]{ }
+                        //new OpenApiSecurityScheme
+                        //{
+                        //    Reference = new OpenApiReference
+                        //    {
+                        //        Type = ReferenceType.SecurityScheme,
+                        //        Id = "Bearer"
+                        //    }
+                        //},
+                        //new string[]{ }
+
+                        securityScheme, new List<string>(){ }
                     }
                 });
             });
